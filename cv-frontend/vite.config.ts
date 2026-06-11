0738ac3a421a881876a08272eae8c35a0b41278c
jopall <jopall@taltech.ee>
2026-06-12 01:15:48 +0300

    useful file

--- cv-frontend/vite.config.ts
+++ cv-frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/team-theta-ai-check/',
   plugins: [react()],
   server: {
     port: 5173
